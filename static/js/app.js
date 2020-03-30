a51f3fcce0a65189fcadced9a85c74c130bf56b6
Nyarai Nhamoinesu <nyarainhamoinesu@gmail.com>
2020-03-30 02:54:47 -0500

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,10 @@
 // Use D3 fetch to read the JSON file
 // The data from the JSON file is arbitrarily named importedData as the argument
-d3.json("data/samples.json").then((importedData) => {
-    // console.log(importedData);
-    var data = importedData;
+//function getBio(samples) {
+    d3.json("samples.json").then((importedData) => {
     console.log(importedData);
-});
+    //var data = importedData;
+})
+
+
+
